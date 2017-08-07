5cf1d6dc8f858a039ebb855e1811d2f1b7265f33
Clexma <1024407215@qq.com>
2017-08-07 17:40:24 +0800

    AutoGenerator update

--- src/test/java/cn/ac/ios/ia/AutoIAGeneratorTest.java
+++ src/test/java/cn/ac/ios/ia/AutoIAGeneratorTest.java
@@ -4,18 +4,26 @@ import cn.ac.ios.machine.ia.IAExporterDOT;
 import cn.ac.ios.machine.ia.InterfaceAutomaton;
 import cn.ac.ios.machine.ia.util.AutoIAGenerator;
 import cn.ac.ios.machine.ia.util.SimulationChecker;
+import cn.ac.ios.words.Alphabet;
 
 public class AutoIAGeneratorTest {
 	public static void main(String[] args){
 		//for(int i = 0; i < 10; i ++){
 		//IAExporterDOT.export(AutoIAGenerator.generate(5));
 		//}
+		Alphabet input = new Alphabet(Integer.class);
+		input.addLetter(0);
+		input.addLetter(1);
 		
+		Alphabet output = new Alphabet(Integer.class);
+        output.addLetter(0);
+        output.addLetter(1);
+        output.addLetter(2);
 		
 		for(int i = 0; i < 100; i++){
 			String[] CE = {""};
-			InterfaceAutomaton oIA = AutoIAGenerator.generate(3);
-			InterfaceAutomaton iIA = AutoIAGenerator.generate(3);
+			InterfaceAutomaton oIA = AutoIAGenerator.generate(input.getAPs(), output.getAPs(), 3);
+			InterfaceAutomaton iIA = AutoIAGenerator.generate(input.getAPs(), output.getAPs(), 3);
 			if(SimulationChecker.alternatingSimCheck(oIA,iIA, CE)){
 				IAExporterDOT.export(oIA);
 				IAExporterDOT.export(iIA);
